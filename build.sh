e67f88f2c6704c98e9aacad3b0a04b3b2795736c
Adrian Dvergsdal <github.com@konto.atmoz.no>
2013-05-28 20:23:31 +0200

    Use different target directory

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# run rigid with default options
-rigid
+# generate web site
+rigid . ../public
 
 # find last modified file and copy as index.html
 last=$(ls -t post/*.md | head -1 | sed -e 's/.md$/.html/')
-cd ../blog.rigid
+cd ../public
 cp "$last" index.html
 
 # remove this file from target
